63f3e077c6b3161984d319553d1b229c24143b5e
Rishith <rishdafish1234@gmail.com>
2020-03-10 18:37:18 -0400

    redid all writing

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -12,3 +11,5 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+from flaskblog import routes
